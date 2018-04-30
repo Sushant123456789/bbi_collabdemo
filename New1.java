8f00adbfd7b2166ecd21becedbcd4f9be70ec8d2
kalyan mazumder <kalyan.mazumder@trickytechnos.com>
2018-04-30 16:05:38 +0530

    Person file added and New1 modified

--- New1.java
+++ New1.java
@@ -1,13 +1,12 @@
 public class New1 {
 
 		public static void main(String[] args) {
-<<<<<<< HEAD
+
 			System.out.println("Hello how are you  sir");
 			System.out.println("Pleasure learning with you");
-=======
-=======
+
 			System.out.println("Hello I am Rishi");
->>>>>>> 9aaff795df7ea7a77f27eb8ae19d6a05a5966c91
+
 		}
 
 }
---
+++ Person.java
@@ -0,0 +1,15 @@
+
+public class Person {
+
+	String name;
+	
+	Person(){
+		this.name="Kalyan";
+	}
+		public static void main(String[] args) {
+
+			System.out.println(name);
+			
+		}
+
+}
